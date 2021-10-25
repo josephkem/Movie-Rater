572757f847da334fdda0418e745f32a2fbb0d778
josephkem <josephkemoy@gmail.com>
2021-10-24 23:37:11 -0400

    Updated the api to update and create objects

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from .models import Movie, Rating
 from .serializers import MovieSerializer, RatingSerializer
@@ -12,8 +13,33 @@ class MovieViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['POST'])
     def rate_movie(self, request, pk=None):
-        res = {"message": "its working"}
-        return Response(res, status=status.HTTP_200_OK)
+        if 'stars' in request.data:
+            movie = Movie.objects.get(id=pk)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
+
+            try:
+                rating = Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                res = {"message": "Rating updated", 'result': serializer.data}
+                return Response(res, status=status.HTTP_200_OK)
+            except:
+                rating = Rating.objects.create(user=user, movie=movie, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                res = {"message": "Rating created", 'result': serializer.data}
+                return Response(res, status=status.HTTP_200_OK)
+
+
+            
+        else:
+            res = {"message": "You need to provide stars"}
+            return Response(res, status=status.HTTP_400_BAD_REQUEST)
+
+
+        
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
